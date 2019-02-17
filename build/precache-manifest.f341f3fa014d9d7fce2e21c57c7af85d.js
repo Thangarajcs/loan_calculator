ea9c3eefd60a979e6564e1d5d58675a521d7bc79
Thangaraj <rajthanga500@gmailk.com>
2019-02-18 00:24:31 +0530

    update-1 on app.js

---
+++ build/precache-manifest.f341f3fa014d9d7fce2e21c57c7af85d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1cf5b2712c3e269738ac",
+    "url": "/static/js/main.1cf5b271.chunk.js"
+  },
+  {
+    "revision": "ef0b7c919df8dbb8013a",
+    "url": "/static/js/2.ef0b7c91.chunk.js"
+  },
+  {
+    "revision": "1cf5b2712c3e269738ac",
+    "url": "/static/css/main.b3302318.chunk.css"
+  },
+  {
+    "revision": "7e36c1199b35cbe07c2eaa90c494176c",
+    "url": "/index.html"
+  }
+];
